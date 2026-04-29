f6313b385b442441dc88c881a9f5cd198afc0553
Lesego-Putswane <lesegoputswane@gmail.com>
2026-04-30 00:06:34 +0200

    added a repetition feature to prompt user if they want to perform another calculation, refactored the code so that it can work well with the latest feature

--- simple_calculator/simple_calculator.py
+++ simple_calculator/simple_calculator.py
@@ -1,3 +1,5 @@
+print("_____Welcome to Segos simple calculator_____\n")
+
 def calculator():
     num1 = float(input("Enter first number: "))
     num2 = float(input("Enter second number: "))
@@ -18,13 +20,29 @@ def calculator():
         if num2 == 0:
             # handling zero division
             return "Division by 0 is invalid! Please enter number greater than 0."
+        else:
+            result = num1 / num2
     else:
-        return "Invalid operator provided! Please enter +, -, *, / "
+        return "Invalid operator provided!"
     
     # refining the result to print out integer if result is an integer
     if result.is_integer():
-        return int(result)
+        return f"{num1} {operator} {num2} = {int(result)}"
     else:
         return result
-    
-print(calculator())
+
+# loop that that controls repetition    
+while True:
+    output = calculator()
+    print(output)
+
+    while True:
+        recalculate = input("\nPerform another calculation? yes(y) / no(n): ").lower()
+
+        if recalculate in ["yes","y"]:
+            break
+        elif recalculate in ["no","n"]:
+            print("See you next time!")
+            exit()
+        else:
+            print("Invalid option please type 'yes/y' or 'no/n'!")
