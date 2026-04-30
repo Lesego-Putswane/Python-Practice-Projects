f416ad96416d39350e6fb784c16f7c4e7f69ebe4
Lesego-Putswane <lesegoputswane@gmail.com>
2026-04-30 15:36:05 +0200

    refactored by practicing seperation by creating functions for each logic

--- simple_calculator/simple_calculator.py
+++ simple_calculator/simple_calculator.py
@@ -1,48 +1,61 @@
 print("_____Welcome to Segos simple calculator_____\n")
 
-def calculator():
+# getting user inputs
+def get_numbers():
     num1 = float(input("Enter first number: "))
     num2 = float(input("Enter second number: "))
     operator = input("Enter an operator you'd like to use (+, -, *, /): ")
 
-    # implementing conditionals as per operators
+    return num1, num2, operator
+
+# implementing conditionals as per operators
+def calculator(num1, num2, operator):
     if operator == "+":
         # additions
-        result = num1 + num2
+        return num1 + num2
     elif operator == "-":
         # subtraction
-        result = num1 - num2
+        return num1 - num2
     elif operator == "*":
         # multiplication
-        result = num1 * num2
+        return num1 * num2
     elif operator == "/":
         # division
         if num2 == 0:
             # handling zero division
             return "Division by 0 is invalid! Please enter number greater than 0."
         else:
-            result = num1 / num2
+            return num1 / num2
     else:
         return "Invalid operator provided!"
-    
-    # refining the result to print out integer if result is an integer
-    if result.is_integer():
-        return f"{num1} {operator} {num2} = {int(result)}"
+
+# refining the result to print out integer if result is an integer   
+def output_format(num1, num2, operator, result):
+    if isinstance(result, float):
+        if result.is_integer():
+            return f"{num1} {operator} {num2} = {int(result)}"
+        else:
+            return result
     else:
         return result
 
-# loop that that controls repetition    
-while True:
-    output = calculator()
-    print(output)
-
+# loop that that controls repetition
+def recalculate():   
     while True:
-        recalculate = input("\nPerform another calculation? yes(y) / no(n): ").lower()
+        num1, num2, operator = get_numbers()
+        result = calculator(num1, num2, operator)
+        output = output_format(num1, num2, operator, result)
+        print(output)
 
-        if recalculate in ["yes","y"]:
-            break
-        elif recalculate in ["no","n"]:
-            print("See you next time!")
-            exit()
-        else:
-            print("Invalid option please type 'yes/y' or 'no/n'!")
+        while True:
+            recalculated = input("\nPerform another calculation? yes(y) / no(n): ").lower()
+
+            if recalculated in ["yes","y"]:
+                break
+            elif recalculated in ["no","n"]:
+                print("See you next time!")
+                exit()
+            else:
+                print("Invalid option please type 'yes/y' or 'no/n'!")
+
+recalculate()
