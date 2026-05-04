70d29408f0e848e0a230bbfd9bb7f68685c1ddd8
Lesego-Putswane <lesegoputswane@gmail.com>
2026-05-04 18:49:25 +0200

    created multiple function to handle different tasks in the weather app

---
+++ weather_app/weather_app.py
@@ -0,0 +1,11 @@
+print("Welcome to the weather app")
+
+def city():
+    user_city = input("Enter city you'd like to get weather for: ")
+    return user_city
+
+def get_weather():
+    pass
+
+def display_weather():
+    pass
